ba531ee3a852ebc61d7bcf326f0805c3026c110e
Ibsa Gilo <ebisa99@yahoo.com>
2020-10-21 02:38:51 -0500

    the first change to Myclass.java file

--- Myclass.java
+++ Myclass.java
@@ -3,5 +3,6 @@ public class Myclass
 	public static void main(String[] args)
 	{
 		System.out.println("here is my first java");
+		System.out.println("This is another change to see if git updates the file.");
 	}
 }
